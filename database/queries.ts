4fff4af2a358d2159906a408268ee336b4f2d4c8
Sebastian Corcino <corcinosebastian@outlook.com>
2024-10-23 22:18:07 -0800

    generalized into db util

--- database/queries.ts
+++ database/queries.ts
@@ -1,31 +1,127 @@
-import { SQLiteDatabase } from "expo-sqlite";
+import { SQLiteDatabase, SQLiteBindParams } from "expo-sqlite";
 
-async function executeByQuery(db: SQLiteDatabase, query: string) {
-  /*
-    Using the connection execute query. 
-    Args:
-        db: The connection instance to cookbook.db.
-        query (str): The SQL statement to run on db.
-    Returns: 
-        list: A list of data retrieved based by query.
-     */
 
-  try {
-    const result = await db.getAllAsync(query);
-    return result;
-  } catch (error) {
-    return [];
+type preGetArgumentType = {
+  columnName?: string;
+  value?: any;
+};
+
+// allow for custom actions
+type getArgumentType = {
+  columnName?: string;
+  action?: "LIKE" | "=";
+  value?: any;
+};
+
+/*
+ * A class for interacting with SQLite database.
+ */
+class DatabaseUtil {
+  private db: SQLiteDatabase;
+  private tableName: string;
+
+  constructor(database: SQLiteDatabase, tableName: string) {
+    this.db = database;
+    this.tableName = tableName;
+  }
+
+  /**
+   * Make fetch queries.
+   * @param {string} query - The SQL statement to run on db.
+   * @params {any[]} [params] - Additional params to include with query.
+   * @returns {Promise<any[]>} -  A promise that resolves to a list of the query results.
+   * If the fetch query fails, an empty array will be returned.
+   */
+  private async fetchByQuery(
+    query: string,
+    params: SQLiteBindParams
+  ): Promise<any[]> {
+    try {
+      const result = await this.db.getAllAsync(query, params);
+      return result;
+    } catch (error) {
+      console.error("Database fetch query failed:", error);
+      return [];
+    }
+  }
+
+  /**
+   * Make write queries: insert, update, delete.
+   * @param {string} query - The SQL statement to run on db.
+   * @params {any[]} [params] - Additional params to include with query.
+   * @returns {Promise<number>} -  A promise that resolves to a number of last inserted row id.
+   * If the write query fails, -1 is returned
+   */
+  public async writeByQuery(
+    query: string,
+    params: SQLiteBindParams
+  ): Promise<number> {
+    try {
+      const result = await this.db.runAsync(query, params);
+      return result.lastInsertRowId;
+    } catch (error) {
+      console.error("Database write query failed:", error);
+      return -1;
+    }
+  }
+
+  public async getAll(argument?: getArgumentType): Promise<any[]> {
+    let query = `SELECT * FROM ${this.tableName}`;
+    const params: string[] = [];
+
+    if (argument) {
+      query += ` WHERE ${argument.columnName} ${argument.action} ?`;
+      params.push(argument.value);
+    }
+    return await this.fetchByQuery(query, params);
   }
 }
 
-export async function getAllMeals(db: SQLiteDatabase) {
-  const query = "SELECT * FROM meals;";
-  const meals = await executeByQuery(db, query);
-  return meals;
+// getAll{Screens} will follow this layout
+// or possibly just pass the table name
+export async function getAllMeals(
+  db: SQLiteDatabase,
+  argument?: preGetArgumentType
+) {
+  const util = new DatabaseUtil(db, "meals");
+
+  if (argument && argument.columnName && argument.value !== undefined) {
+    return await util.getAll({
+      columnName: argument.columnName,
+      action: "LIKE",
+      value: `%${argument.value}%`,
+    });
+  }
+  return await util.getAll();
 }
 
-export async function getShoppingCart(db: SQLiteDatabase) {
-  const query = "SELECT * FROM shopping_cart;";
-  const cart = await executeByQuery(db, query);
-  return cart;
+export async function getMeal(
+  db: SQLiteDatabase,
+  argument: preGetArgumentType
+) {
+  const util = new DatabaseUtil(db, "meals");
+  return await util.getAll({
+    columnName: argument.columnName,
+    action: "=",
+    value: argument.value,
+  });
+}
+
+export async function getAllShoppingCart(db: SQLiteDatabase) {
+  const util = new DatabaseUtil(db, "shopping_cart");
+  return await util.getAll();
+}
+
+export async function setInCart(
+  db: SQLiteDatabase,
+  params: { mealId: number; productId: number }
+) {
+  const query = `
+    UPDATE shopping_cart
+    SET in_cart = 1
+    WHERE meal_id = ? AND product_id = ?;
+  `;
+
+  const util = new DatabaseUtil(db, "shopping_cart");
+  return await util.writeByQuery(query, [params.mealId, params.productId]);
 }
