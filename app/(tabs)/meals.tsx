a5ddf1602adda13e6159a17b0b700dc4c07f2a03
Sebastian Corcino <corcinosebastian@outlook.com>
2024-10-29 09:41:02 -0800

    add the ability to get meals, delete meals, and add meals

--- app/(tabs)/meals.tsx
+++ app/(tabs)/meals.tsx
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import { SearchBar } from 'react-native-elements';
+import React, { useState, useEffect } from "react";
+import { SearchBar } from "react-native-elements";
 import {
   Alert,
   Modal,
@@ -9,49 +9,71 @@ import {
   View,
   TextInput,
   StatusBar,
-} from 'react-native';
-import ItemList from '../../components/ItemList'; // ItemList component
+} from "react-native";
+import ItemList from "../../components/ItemList";
+import { useSQLiteContext } from "expo-sqlite";
+import { deleteEntry, getAll, insertEntry } from "@/database/queries";
+import { getDateNow } from "@/database/utils";
 
 interface Meal {
-  id: string;
-  title: string;
+  meal_id: number;
+  create_at: string;
+  update_at: string;
+  photo_url: string;
+  name: string;
+  description: string;
 }
 
 const MealsScreen: React.FC = () => {
   const [modalVisible, setModalVisible] = useState(false);
-  const [mealName, setMealName] = useState('');
-  const [searchQuery, setSearchQuery] = useState('');
+  const [newMealName, setNewMealName] = useState<string>(""); // temp store for adding new meals
+  const [searchQuery, setSearchQuery] = useState("");
   const [meals, setMeals] = useState<Meal[]>([]);
+  const db = useSQLiteContext();
 
-  const addMeal = () => {
-    if (mealName.trim() === '') {
-      Alert.alert('Enter a meal name');
+  useEffect(() => {
+    const fetchMeals = async () => {
+      const fetchedMeals = await getAll(db, "meals");
+      setMeals(fetchedMeals);
+    };
+    fetchMeals();
+  }, [db]);
+
+  const addMeal = async () => {
+    if (newMealName.trim() === "") {
+      Alert.alert("Enter a meal name");
       return;
     }
+    console.log("Adding meal:", newMealName);
 
-    console.log('Adding meal:', mealName);
+    const newMeal = {
+      create_at: getDateNow(),
+      update_at: getDateNow(),
+      photo_url: null,
+      name: newMealName,
+      description: "",
+    };
+    insertEntry(db, "meals", newMeal);
 
-    setMeals((prevMeals) => [
-      ...prevMeals,
-      { id: Date.now().toString(), title: mealName },
-    ]);
+    const fetchedMeals = await getAll(db, "meals");
+    setMeals(fetchedMeals);
 
-    setMealName('');
-    setSearchQuery(''); // clear search query to make sure the new meal is visible
+    setNewMealName("");
+    setSearchQuery(""); // clear search query to make sure the new meal is visible
     setModalVisible(false);
   };
 
   const confirmDeleteMeal = (meal: Meal) => {
     Alert.alert(
-      'Delete Meal',
-      `Are you sure you want to delete ${meal.title}?`,
+      "Delete Meal",
+      `Are you sure you want to delete ${meal.name}?`,
       [
         {
-          text: 'Cancel',
-          style: 'cancel',
+          text: "Cancel",
+          style: "cancel",
         },
         {
-          text: 'Delete',
+          text: "Delete",
           onPress: () => deleteMeal(meal),
         },
       ],
@@ -59,8 +81,14 @@ const MealsScreen: React.FC = () => {
     );
   };
 
-  const deleteMeal = (meal: Meal) => {
-    setMeals((prevMeals) => prevMeals.filter((m) => m.id !== meal.id));
+  const deleteMeal = async (meal: Meal) => {
+    deleteEntry(db, "meals", {
+      columnName: "meal_id",
+      action: "=",
+      value: meal.meal_id,
+    });
+    const fetchedMeals = await getAll(db, "meals");
+    setMeals(fetchedMeals);
   };
 
   // Function to handle search query change
@@ -82,9 +110,9 @@ const MealsScreen: React.FC = () => {
         items={meals}
         searchQuery={searchQuery}
         onDeleteItem={confirmDeleteMeal}
-        getTitle={(item) => item.title} // provides a way to extract title from Meal
+        getTitle={(item) => item.name} // provides a way to extract title from Meal
       />
-      
+
       <View style={styles.buttonContainer}>
         <Pressable
           style={[styles.button, styles.buttonOpen]}
@@ -106,11 +134,11 @@ const MealsScreen: React.FC = () => {
           <View style={styles.modalView}>
             <Text style={styles.modalText}>Enter Meal Name:</Text>
             <TextInput
-              style={[styles.input, { textAlign: 'center' }]}
+              style={[styles.input, { textAlign: "center" }]}
               placeholder="e.g. Spaghetti"
               placeholderTextColor="#808080"
-              value={mealName}
-              onChangeText={setMealName}
+              value={newMealName}
+              onChangeText={setNewMealName}
             />
             <View style={styles.buttonContainerHorizontal}>
               <Pressable
@@ -137,51 +165,51 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     marginTop: StatusBar.currentHeight || 0,
-    backgroundColor: '#F0EAD6',
+    backgroundColor: "#F0EAD6",
   },
   searchBarContainer: {
-    backgroundColor: '#F0EAD6',
+    backgroundColor: "#F0EAD6",
     borderBottomWidth: 0,
     borderTopWidth: 0,
     marginBottom: 10,
   },
   searchInputContainer: {
-    backgroundColor: '#d3d3d3',
+    backgroundColor: "#d3d3d3",
   },
   buttonContainer: {
     padding: 10,
-    justifyContent: 'flex-end',
-    alignItems: 'center',
+    justifyContent: "flex-end",
+    alignItems: "center",
   },
   button: {
     borderRadius: 21,
     padding: 18,
     marginVertical: 3,
-    width: '45%',
+    width: "45%",
   },
   buttonOpen: {
-    backgroundColor: '#36454F',
-    justifyContent: 'flex-end',
+    backgroundColor: "#36454F",
+    justifyContent: "flex-end",
   },
   buttonClose: {
-    backgroundColor: '#36454F',
+    backgroundColor: "#36454F",
   },
   centeredView: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
     marginTop: 22,
   },
   modalView: {
     margin: 20,
-    backgroundColor: 'white',
+    backgroundColor: "white",
     borderRadius: 21,
     padding: 35,
-    alignItems: 'center',
+    alignItems: "center",
     height: 250,
     width: 300,
-    justifyContent: 'space-evenly',
-    shadowColor: '#000',
+    justifyContent: "space-evenly",
+    shadowColor: "#000",
     shadowOffset: {
       width: 0,
       height: 2,
@@ -191,25 +219,25 @@ const styles = StyleSheet.create({
   },
   input: {
     height: 40,
-    borderColor: 'gray',
+    borderColor: "gray",
     borderWidth: 1,
     marginBottom: 15,
-    width: '100%',
+    width: "100%",
     paddingHorizontal: 10,
   },
   buttonContainerHorizontal: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    width: '100%',
+    flexDirection: "row",
+    justifyContent: "space-between",
+    width: "100%",
   },
   textStyle: {
-    color: 'white',
-    fontWeight: 'bold',
-    textAlign: 'center',
+    color: "white",
+    fontWeight: "bold",
+    textAlign: "center",
   },
   modalText: {
     marginBottom: 25,
-    textAlign: 'center',
+    textAlign: "center",
     fontSize: 24,
   },
 });
---
+++ database/utils.ts
@@ -0,0 +1,14 @@
+/* Get the current data and time in UTC
+ * format: YYYY-MM-DD HH:MM:SS
+ */
+export function getDateNow() {
+  const now = new Date();
+  const year = now.getUTCFullYear();
+  const month = String(now.getUTCMonth() + 1).padStart(2, "0");
+  const day = String(now.getUTCDate()).padStart(2, "0");
+  const hours = String(now.getUTCHours()).padStart(2, "0");
+  const minutes = String(now.getUTCMinutes()).padStart(2, "0");
+  const seconds = String(now.getUTCSeconds()).padStart(2, "0");
+
+  return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
+}
